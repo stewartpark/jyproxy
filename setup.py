7a86816eb7b65f4e8d921a7abca6854e7b0d8327
Stewart Park <stewartpark92@gmail.com>
2014-10-25 18:55:38 +0900

    Intermediate commit.

--- setup.py
+++ setup.py
@@ -5,15 +5,20 @@ from distutils.core import setup
 import py2exe
 
 setup(
-        console=['app.py'],
+        console=[
+            {
+                'script': 'app.py',
+                'icon_resources':[ (0,'./img/icon.ico') ]
+            }
+        ],
         options={
                 'py2exe': { 
                     'compressed': 1,
                     'optimize': 2,
                     'dist_dir': './dist',
-                    'bundle_files': 1
+                    'bundle_files': 1,
+                    'includes': ['twisted']
                     }
         },
         zipfile = None,
-        icon_resource='./img/icon.ico'
     )
